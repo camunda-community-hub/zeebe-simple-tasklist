fa312a47dd1bd937f51856574f373458016c0a26
Sebastian Menski <sebastian.menski@camunda.com>
2019-07-15 14:42:07 +0200

    chore(ci): migrate to cloud CI

---
+++ .ci/scripts/github-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xeu
+
+cd target
+
+export PROJECT_NAME="Zeebe DMN Worker"
+export GITHUB_TOKEN=${GITHUB_TOKEN_PSW}
+export GITHUB_ORG=zeebe-io
+export GITHUB_REPO=zeebe-simple-tasklist
+export ARTIFACT=zeebe-simple-tasklist-${RELEASE_VERSION}.jar
+export CHECKSUM=${ARTIFACT}.sha1sum
+
+# create checksum files
+sha1sum ${ARTIFACT} > ${CHECKSUM}
+
+# do github release
+curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar xjvf - --strip 3
+
+./github-release release --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --draft --name "${PROJECT_NAME} ${RELEASE_VERSION}" --description ""
+./github-release upload --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --name "${ARTIFACT}" --file "${ARTIFACT}"
+./github-release upload --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --name "${CHECKSUM}" --file "${CHECKSUM}"
